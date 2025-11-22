c3d95254258344848e2e3bf69cfd688faed8ad64
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-22 11:03:09 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 150 ms, Memory: 39,312 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,28 +1,26 @@
 
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Solution {
-	public static void main(String[] args) throws FileNotFoundException {
-
+	public static void main(String[] args) throws Exception {
+	
 		Scanner sc = new Scanner(System.in);
+
 		int T = 10;
-		for(int z = 1 ; z<= T; z++) {
+		for (int z = 1; z <= T; z++) {
 			int n = sc.nextInt();
-			int [] arr = new int[n];
-			for(int i = 0 ; i < n ; i ++) {
-				arr[i] = sc.nextInt();
-			}
 			int total = 0;
-			for(int i = 2; i < n - 2 ; i++) {
-				if(arr[i-1] < arr[i] && arr[i - 2] < arr[i] && arr[i] > arr[i+1] && arr[i] > arr[i+2]) {
-					int max = Math.max(arr[i-1], arr[i-2]);
-					max = Math.max(max, arr[i+1]);
-					max = Math.max(max, arr[i+2]);
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++)
+				arr[i] = sc.nextInt();
+			for (int i = 2; i < n - 2; i++) {
+				if (arr[i] > arr[i - 1] && arr[i] > arr[i - 2] && arr[i] > arr[i + 1] && arr[i] > arr[i + 2]) {
+					int max = Math.max(arr[i - 1], arr[i - 2]);
+					max = Math.max(max, arr[i + 1]);
+					max = Math.max(max, arr[i + 2]);
+
 					total += arr[i] - max;
 				}
 			}
