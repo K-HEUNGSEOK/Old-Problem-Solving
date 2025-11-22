65105fae82eb51663bfd5f4f67117eb9b378d3d9
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-22 12:00:15 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 117 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -1,31 +1,31 @@
+
+
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
+	public static void main(String[] args) throws Exception {
 
-  public static void main(String[] args) throws FileNotFoundException {
-    
-    Scanner sc = new Scanner(System.in);
-    int T = 10;
-    for (int z = 1; z <= T; z++) {
-      int trash = sc.nextInt();
-      String findString = sc.next();
-      String str = sc.next();
-      int ans = 0;
-
-      int len = str.length();
-      for (int i = 0; i < len; i++) {
-        int index = str.indexOf(findString);
-        if (index == -1) {
-          break;
-        } else {
-          ans++;
-          str = str.substring(index + 1);
-        }
-      }
-      System.out.println("#" + z + " " + ans);
-    }
-  }
+		Scanner sc = new Scanner(System.in);
+		
+		int T = 10;
+		for(int z = 1; z<= T; z ++) {
+			int trash = sc.nextInt();
+			String target = sc.next();
+			String str = sc.next();
+			int ans =0;
+			
+			while(true) {
+				int index = str.indexOf(target);
+			
+				if(index == -1) {
+					break;
+				}
+				ans ++;
+				str = str.substring(index+1);
+				
+			}
+			System.out.println("#" +  z+ " " +ans);
+		}
+	}
 }
-
