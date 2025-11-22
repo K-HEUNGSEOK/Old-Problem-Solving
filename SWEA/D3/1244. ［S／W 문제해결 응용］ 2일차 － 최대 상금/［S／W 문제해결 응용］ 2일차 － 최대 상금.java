c2df98bd59c5dc81223603dd8ba9eeb0723ff7d6
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-22 11:19:23 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 1,119 ms, Memory: 273,564 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -1,52 +1,53 @@
 
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Solution {
-	static int cnt, total;
-	static char[] arr;
-	static boolean[][] visited ;
-	public static void main(String[] args) throws FileNotFoundException {
+	static int num, cnt, ans;
+	static boolean[][] ch;
 
+	public static void main(String[] args) throws Exception {
+	
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for(int z = 1 ; z<= T; z++) {
-			//한번씩 다 바꿔보면서 최대값을 찾는 것 
-			
-			int n = sc.nextInt();
-			arr = (n+"").toCharArray();
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			num = sc.nextInt();
 			cnt = sc.nextInt();
-			visited = new boolean[cnt+1][1000000];
-			total = 0;
-			DFS(0);
-			System.out.println("#"+z+" " + total);
+			ans = 0;
+			ch = new boolean[1000001][cnt + 1];
+			DFS(num, 0);
+			System.out.println("#" + (i+1)+ " " + ans);
 		}
 	}
-	static void DFS(int c) {
-		int currentNumber = Integer.parseInt(new String(arr));
-		if(visited[c][currentNumber]) return;
-		
-		visited[c][currentNumber] = true;
-		if(c == cnt) {
-			total = Math.max(total, currentNumber);
+
+	static void DFS(int num, int c) {
+
+		if (c == cnt) {
+			if (ans > num)
+				return;
+			ans = Math.max(ans, num);
 			return;
-		}else {
-			for(int i = 0 ; i < arr.length; i ++) {
-				for(int j = i + 1; j < arr.length; j ++) {
-					char temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-					DFS(c +1);
-					temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
+		}
+		if (ch[num][c])
+			return;
+
+		ch[num][c] = true;
+
+		char[] arr = (num + "").toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+
+				char temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+
+				DFS(Integer.parseInt(new String(arr)), c + 1);
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
 			}
 		}
+
 	}
-	
 }
