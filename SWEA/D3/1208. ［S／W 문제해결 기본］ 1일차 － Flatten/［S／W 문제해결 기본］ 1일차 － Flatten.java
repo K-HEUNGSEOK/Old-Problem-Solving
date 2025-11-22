6ec64d1335663454a0006f95f1fcf11b5fd08e6a
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-11-22 11:41:15 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 144 ms, Memory: 31,056 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,38 +1,31 @@
 
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Solution {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws Exception {
 
 		Scanner sc = new Scanner(System.in);
-		int T = 10;
-		for (int z = 1; z <= T; z++) {
-			int n = sc.nextInt();
-			int[] arr = new int[100];
-			for (int i = 0; i < 100; i++) {
-				arr[i] = sc.nextInt();
-			}
-			// 최저와 최고 높이를 계속 갱신하면서 -- ++ 을 해준다
-			// 가장 높은곳과 낮은곳의 차이가 1이내면 평탄화를 그만한다
-			// 최고점과 최저점의 차이를 낸다
-
-			while (n-- > 0) {
-				Arrays.sort(arr);
-				arr[0]++;
-				arr[99]--;
-				int total = arr[99] - arr[0];
 		
-				if (total <= 1) {
-					break;
-				}
+		int T = 10;
+		for(int z = 1; z<= T; z ++) {
+		int n = sc.nextInt();
+		int [] arr = new int[100];
+		for(int i = 0 ; i < 100 ; i ++) arr[i] = sc.nextInt();
+		int cnt = 0;
+		int temp = n;
+		Arrays.sort(arr);
+		while(n -- > 0) {
+			arr[0] ++;
+			arr[99] --;
+			if(arr[99] - arr[0] <= 1) {
+				break;
 			}
 			Arrays.sort(arr);
-			System.out.println("#" + z + " " + (arr[99] - arr[0]));
+		}
+		Arrays.sort(arr);
+		System.out.println("#" + z+ " " + (arr[99] - arr[0]));
 		}
 	}
 }
